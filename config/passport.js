6c1eaba19d48dd5c2cb46453d4003de895b2d498
Muthu-Priyadarshini-A <priyaarumugam2303@gmail.com>
2024-01-23 18:43:23 +0530

    first commit

---
+++ config/passport.js
@@ -0,0 +1,90 @@
+const passport = require("passport");
+const User = require("../models/user");
+const LocalStrategy = require("passport-local").Strategy;
+
+// authentication using passport
+passport.use(
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passReqToCallback: true,
+      },
+      async function (req, email, password, done) {
+        try {
+          // find the user and establish the identity
+          const user = await User.findOne({ email });
+  
+          if (!user) {
+            console.log("Invalid UserName or Password");
+            return done(null, false);
+          }
+  
+          // match the Password
+          const isPasswordValid = await user.isValidatePassword(password);
+  
+          if (!isPasswordValid) {
+            console.log("Invalid Username or Password");
+            return done(null, false);
+          }
+  
+          return done(null, user);
+        } catch (err) {
+          console.log("Error in finding the user", err);
+          return done(err);
+        }
+      }
+    )
+  );
+
+// serializing the user to choose which key should be kept in cookies
+passport.serializeUser(function (user, done) {
+  return done(null, user.id);
+});
+
+// deserializing the user form the key in the cookies
+// passport.deserializeUser(function (id, done) {
+//   User.findById(id, function (err, user) {
+//     if (err) {
+//       console.log("Error in finding user ---> Passport");
+//       return done(err);
+//     }
+
+//     return done(null, user);
+//   });
+// });
+passport.deserializeUser(async function (id, done) {
+    try {
+      const user = await User.findById(id);
+  
+      if (!user) {
+        console.log("User not found during deserialization");
+        return done(null, false);
+      }
+  
+      return done(null, user);
+    } catch (err) {
+      console.log("Error in finding user during deserialization", err);
+      return done(err);
+    }
+  });
+
+// check if user authenticated
+passport.checkAuthentication = function (req, res, next) {
+  // if the user is signed in, then pass on the request to the next function
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  // redirecting the user
+  return res.redirect("/");
+};
+
+passport.setAuthenticatedUser = function (req, res, next) {
+  // if user is authenticated that store the user in req
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user;
+  }
+  next();
+};
+
+module.exports = passport;
---
+++ controllers/studentController.js
@@ -0,0 +1,129 @@
+const Interview = require("../models/interview");
+const Student = require("../models/student");
+
+// render add student page
+module.exports.addStudent = (req, res) => {
+  if (req.isAuthenticated()) {
+    return res.render("add_student", {
+      title: "Add Student",
+    });
+  }
+
+  return res.redirect("/");
+};
+
+// render edit student page
+module.exports.editStudent = async (req, res) => {
+  const student = await Student.findById(req.params.id);
+
+  if (req.isAuthenticated()) {
+    return res.render("edit_student", {
+      title: "Edit Student",
+      student_details: student,
+    });
+  }
+
+  return res.redirect("/");
+};
+
+// creating a new Student
+module.exports.create = async (req, res) => {
+    try {
+      const {
+        name,
+        email,
+        batch,
+        college,
+        placementStatus,
+        dsa_score,
+        react_score,
+        webdev_score,
+      } = req.body;
+  
+      // check if student already exists
+      const existingStudent = await Student.findOne({ email });
+  
+      if (!existingStudent) {
+        const newStudent = await Student.create({
+          name,
+          email,
+          college,
+          batch,
+          dsa_score,
+          react_score,
+          webdev_score,
+          placementStatus,
+        });
+  
+        return res.redirect("back");
+      } else {
+        return res.redirect("back");
+      }
+    } catch (err) {
+      console.log(err);
+      return res.redirect("back"); // Handle the error as needed
+    }
+  };
+// Deletion of student
+module.exports.destroy = async (req, res) => {
+    try {
+      const { studentId } = req.params;
+      const student = await Student.findById(studentId);
+  
+      if (!student) {
+        return res.redirect("back");
+      }
+  
+      const interviewsOfStudent = student.interviews;
+  
+      // Delete reference of student from companies in which this student is enrolled
+      if (interviewsOfStudent.length > 0) {
+        for (let interview of interviewsOfStudent) {
+          await Interview.findOneAndUpdate(
+            { company: interview.company },
+            { $pull: { students: { student: studentId } } }
+          );
+        }
+      }
+  
+      await Student.deleteOne({ _id: studentId }); // Use await here to make sure the student is removed before redirecting
+      return res.redirect("back");
+    } catch (err) {
+      console.log("error", err);
+      return res.redirect("back"); // Handle the error as needed
+    }
+  };
+
+// update student details
+module.exports.update = async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id);
+    const {
+      name,
+      college,
+      batch,
+      dsa_score,
+      react_score,
+      webdev_score,
+      placementStatus,
+    } = req.body;
+
+    if (!student) {
+      return res.redirect("back");
+    }
+
+    student.name = name;
+    student.college = college;
+    student.batch = batch;
+    student.dsa_score = dsa_score;
+    student.react_score = react_score;
+    student.webdev_score = webdev_score;
+    student.placementStatus = placementStatus;
+
+    student.save();
+    return res.redirect("/dashboard");
+  } catch (err) {
+    console.log(err);
+    return res.redirect("back");
+  }
+};
